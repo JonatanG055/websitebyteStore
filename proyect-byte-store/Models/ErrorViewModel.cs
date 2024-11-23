09872c2ec5b94c67dfc56e1c0358a9bf8c06b62c
Jonatan <jonataneliasguevarachicas129@gmail.com>
2024-11-23 03:36:47 -0600

    proyect intregret

---
+++ proyect-byte-store/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyect_byte_store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
