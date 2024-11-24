c5d38f8eba78aa26ca05d9c9280f78377c53a801
Jonatan <jonataneliasguevarachicas129@gmail.com>
2024-11-23 19:05:45 -0600

    the message edit of welcome

--- obj/Debug/net8.0/websitebyteStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/websitebyteStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("websitebyteStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b66fcc5ee5a8a63dbf67cde4c7a8981e2e1fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6717a4a7c9acfe5804513596277d0c9178e4ecd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("websitebyteStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("websitebyteStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
